8fce0d4eb9612716984c93b864af56b96e3e6fb4
vaibhavkhot1709 <vaibhavkhot1617gmail.com>
2023-06-24 16:23:51 +0530

    final commit

--- new_rest_api/src/main/java/com/new_rest_api/service/StdServiceImpl.java
+++ new_rest_api/src/main/java/com/new_rest_api/service/StdServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.new_rest_api.dao.StdDao;
 import com.new_rest_api.exception.InvaliStudentException;
